4d40469f423d0d67a04192e022356a3eb46e1137
David Suckstorff <dsucksto@thoughtworks.com>
2020-02-26 19:15:10 -0600

    Add zsh_completions when configuring zsh

--- config/scripts/configure_zsh.sh
+++ config/scripts/configure_zsh.sh
@@ -3,3 +3,5 @@
 # TODO: the zsh install script commandeers the terminal, need to find a way to have it not do that
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
